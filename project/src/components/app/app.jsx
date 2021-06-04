82297411d6ebff3948e4f6c97ab698d5eba2d5de
Адель <selly.lady.zakirova@gmail.com>
2021-06-04 17:38:46 +0500

    Добавляем React Router

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+
 import MainPage from '../main-page/main-page';
+import PageFavorites from '../page-favorites/page-favorites';
+import PageOfferProperty from '../page-offer-property/page-offer-property';
+import PageLogin from '../page-login/page-login';
+import PageNotFound from '../page-not-found/page-not-found';
+
+import {AppRoute} from '../../const';
 
 function App(props) {
   const {cardsCount} = props;
 
   return (
-    <MainPage cardsCount={cardsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.MAIN}>
+          <MainPage
+            cardsCount={cardsCount}
+          />
+        </Route>
+        <Route exact path={AppRoute.SIGN_IN}>
+          <PageLogin />
+        </Route>
+        <Route exact path={AppRoute.FAVORITES}>
+          <PageFavorites />
+        </Route>
+        <Route exact path={AppRoute.ROOM}>
+          <PageOfferProperty />
+        </Route>
+        <Route>
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
