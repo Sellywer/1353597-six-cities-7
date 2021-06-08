01b517b5fff4175e476357c4b795d8964104a38e
Адель <selly.lady.zakirova@gmail.com>
2021-06-08 22:52:52 +0500

    Добавит моки для карточек с предложениями.

---
+++ project/src/components/mock/offers.js
@@ -0,0 +1,143 @@
+const offers = [
+  {
+    bedrooms: 3,
+    city: {
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10,
+      },
+      name: 'Amsterdam',
+    },
+    description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    goods: ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
+    host: {
+      avatarUrl: 'avatar-angelina.jpg',
+      id: 1,
+      isPro: true,
+      name: 'Angelina',
+    },
+    id: 1,
+    images: ['img/1.png', 'img/2.png'],
+    isFavorite: false,
+    isPremium: false,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8,
+    },
+    maxAdults: 4,
+    previewImage: 'img/1.png',
+    price: 120,
+    rating: 4.8,
+    title: 'Beautiful & luxurious studio at great location',
+    type: 'apartment',
+  },
+
+  {
+    bedrooms: 2,
+    city: {
+      location: {
+        latitude: 48.8534,
+        longitude: 2.3488,
+        zoom: 10,
+      },
+      name: 'Paris',
+    },
+    description: 'One of the most famous cities in the world, Paris captivates guests from the first minute, at a glance.',
+    goods: ['Attic', 'Kitchen', 'Telescope', 'Washing machine', 'Coffee machine', 'Jacuzzi'],
+    host: {
+      avatarUrl: 'avatar-max.jpg',
+      id: 2,
+      isPro: true,
+      name: 'Max',
+    },
+    id: 2,
+    images: ['img/3.png', 'img/4.png'],
+    isFavorite: true,
+    isPremium: true,
+    location: {
+      latitude: 48.8534,
+      longitude: 2.3488,
+      zoom: 8,
+    },
+    maxAdults: 3,
+    previewImage: 'img/3.png',
+    price: 340,
+    rating: 4.9,
+    title: 'This hotel is a symbol of love and romance, fashion and sophistication.',
+    type: 'hotel',
+  },
+
+  {
+    bedrooms: 6,
+    city: {
+      location: {
+        latitude: 55.67594,
+        longitude: 12.56553,
+        zoom: 10,
+      },
+      name: 'Copenhagen ',
+    },
+    description: 'The world famous sculpture "The Little Mermaid" is located in Copenhagen.',
+    goods: ['Cable TV', 'Kitchen', 'Round Bed', 'Dishwasher', 'Coffee machine', 'Park View'],
+    host: {
+      avatarUrl: 'avatar-miranda.jpg',
+      id: 3,
+      isPro: true,
+      name: 'Miranda',
+    },
+    id: 3,
+    images: ['img/5.png', 'img/6.png'],
+    isFavorite: true,
+    isPremium: false,
+    location: {
+      latitude: 55.67594,
+      longitude: 12.56553,
+      zoom: 8,
+    },
+    maxAdults: 10,
+    previewImage: 'img/5.png',
+    price: 180,
+    rating: 4.6,
+    title: 'Christianshavn is a good choice for travelers interested in design, atmosphere and culture.',
+    type: 'house',
+  },
+
+  {
+    bedrooms: 1,
+    city: {
+      location: {
+        latitude: 42.69771,
+        longitude: 23.32187,
+        zoom: 10,
+      },
+      name: 'Sofia',
+    },
+    description: 'On the coat of arms of Sofia it is written - "It grows, but does not age".',
+    goods: ['TV', 'Kitchen', 'Bath', 'Coffee machine', 'Breakfast'],
+    host: {
+      avatarUrl: 'avatar-katerina.jpg',
+      id: 4,
+      isPro: false,
+      name: 'Katerina',
+    },
+    id: 4,
+    images: ['img/7.png', 'img/8.png'],
+    isFavorite: true,
+    isPremium: false,
+    location: {
+      latitude: 42.69771,
+      longitude: 23.32187,
+      zoom: 8,
+    },
+    maxAdults: 3,
+    previewImage: 'img/7.png',
+    price: 130,
+    rating: 4.3,
+    title: 'The room is located at the foot of the mountains.',
+    type: 'room',
+  },
+];
+
+export default offers;
