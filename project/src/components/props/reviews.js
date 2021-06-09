4b8990f487f458a02ab1246e05b1c6f97a662b37
Адель <selly.lady.zakirova@gmail.com>
2021-06-09 21:54:49 +0500

    Подключит моки в App и создаст отдельные модули для описания валидации пропсов.

---
+++ project/src/components/props/reviews.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+
+export const reviews = PropTypes.shape({
+  comment: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+  host: PropTypes.shape({
+    avatarUrl: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    isPro: PropTypes.bool.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+});
--- project/src/index.js
+++ project/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import offers from './components/mock/offers';
+import reviews from './components/mock/reviews';
+
 
 const Setting = {
   CARDS_COUNT: 5,
@@ -10,6 +13,8 @@ ReactDOM.render(
   <React.StrictMode>
     <App
       cardsCount={Setting.CARDS_COUNT}
+      offers={offers}
+      reviews={reviews}
     />
   </React.StrictMode>,
   document.getElementById('root'));
