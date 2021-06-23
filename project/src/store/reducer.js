f1565f2218b8b5fe18cc1d945ce51c1df7a8f862
Адель <selly.lady.zakirova@gmail.com>
2021-06-23 16:36:15 +0500

    Создаст действия и редьюсер.

---
+++ project/src/store/reducer.js
@@ -0,0 +1,29 @@
+import {ActionType} from './action';
+import offers from '../mocks/offers';
+
+const DEFAULT_CITY = 'Paris';
+
+const initialState = {
+  city: DEFAULT_CITY,
+  offers: offers,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.CHANGE_CITY:
+      return {
+        ...state,
+        city: action.payload,
+      };
+    case ActionType.FILTER_OFFERS:
+      return {
+        ...state,
+        offers: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+
+export {reducer};
